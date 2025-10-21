f948c346bc473f649717c8e4f0a6d37e1720608e
nc-BobLee <bo.o.li@intel.com>
2025-10-21 16:05:56 +0800

    remove unnecessary words.

--- examples/stable-diffusion/run_kolors_demo.sh
+++ examples/stable-diffusion/run_kolors_demo.sh
@@ -4,6 +4,6 @@ export PT_HPU_LAZY_MODE=1
 export PT_HPU_GPU_MIGRATION=1
 
 python3 ./text_to_image_kolors.py \
-    --model_name_or_path '/mnt/ceph1/libo/hf_models/Kolors' \
+    --model_name_or_path 'Kwai-Kolors/Kolors' \
     --prompts "一张瓢虫的照片，微距，变焦，高质量，电影，拿着一个牌子，写着“可图”" \
 
