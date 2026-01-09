23f8549c31dc2010dd9e92768d941fcf57327a6e
tianyuan211 <paulyuen@umich.edu>
2026-01-09 18:59:03 +0800

    Update attention_processor.py

--- optimum/habana/diffusers/models/attention_processor.py
+++ optimum/habana/diffusers/models/attention_processor.py
@@ -806,9 +806,12 @@ class GaudiFlux2AttnProcessor:
 
         # Fast FSDPA is not supported in training mode
         fsdpa_mode = "None" if self.is_training else "fast"
-        hidden_states = self.fav3.forward(query, key, value, attention_mask=attention_mask, fsdpa_mode=fsdpa_mode)
-        
-        hidden_states = hidden_states.flatten(2, 3)
+        # hidden_states = self.fav3.forward(query, key, value, attention_mask=attention_mask, fsdpa_mode=fsdpa_mode)
+        hidden_states = FusedSDPA.apply(
+            query.transpose(1, 2), key.transpose(1, 2), value.transpose(1, 2), None, 0.0, False, None, fsdpa_mode, None
+        )
+
+        hidden_states = hidden_states.transpose(1, 2).flatten(2, 3)
         hidden_states = hidden_states.to(query.dtype)
 
         if encoder_hidden_states is not None:
@@ -881,9 +884,12 @@ class GaudiFlux2ParallelSelfAttnProcessor:
 
         # Fast FSDPA is not supported in training mode
         fsdpa_mode = "None" if self.is_training else "fast"
-        hidden_states = self.fav3.forward(query, key, value, attention_mask=attention_mask, fsdpa_mode=fsdpa_mode)
-        
-        hidden_states = hidden_states.flatten(2, 3)
+        # hidden_states = self.fav3.forward(query, key, value, attention_mask=attention_mask, fsdpa_mode=fsdpa_mode)
+        hidden_states = FusedSDPA.apply(
+            query.transpose(1, 2), key.transpose(1, 2), value.transpose(1, 2), None, 0.0, False, None, fsdpa_mode, None
+        )
+
+        hidden_states = hidden_states.transpose(1, 2).flatten(2, 3)
         hidden_states = hidden_states.to(query.dtype)
 
         # Handle the feedforward (FF) logic
