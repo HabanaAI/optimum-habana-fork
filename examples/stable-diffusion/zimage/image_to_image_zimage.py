b804f2846d3206855aaf09752f0a81c564481270
nc-BobLee <bo.o.li@intel.com>
2026-01-21 18:31:01 +0800

    simplify Z-image examples and add README

--- examples/stable-diffusion/zimage/image_to_image_zimage.py
+++ examples/stable-diffusion/zimage/image_to_image_zimage.py
@@ -9,7 +9,7 @@ from diffusers import ZImageControlNetModel
 from diffusers.utils import load_image
 
 from optimum.habana.transformers.gaudi_configuration import GaudiConfig
-from optimum.habana.diffusers import GaudiStableDiffusionZImageControlNetInpaintPipeline
+from optimum.habana.diffusers import GaudiStableDiffusionZImageControlNetPipeline, GaudiStableDiffusionZImageControlNetInpaintPipeline
 
 
 def set_seed(seed):
@@ -36,6 +36,12 @@ def main():
         default="inpaint.jpg",
         type=str,
         help="Z-image controlnet inpaint image path",
+    )
+    parser.add_argument(
+        "--pipeline_type",
+        default="controlnet",
+        type=str,
+        help="Z-image controlnet pipeline type",
     )
     parser.add_argument(
         "--mask_path",
@@ -113,47 +119,73 @@ def main():
     }
     model_name_path = args.model_name_or_path
 
+    #config='hlky/Z-Image-Turbo-Fun-Controlnet-Union-2.1/config.json',
     controlnet = ZImageControlNetModel.from_single_file(
             args.controlnet_path,
-            config='hlky/Z-Image-Turbo-Fun-Controlnet-Union-2.1/config.json',
+            config='config.json',
             torch_dtype = torch.bfloat16
     )
 
     # 1. Load the pipeline
     # Use bfloat16 for optimal performance on supported GPUs
-    pipe = GaudiStableDiffusionZImageControlNetInpaintPipeline.from_pretrained(
-            model_name_path, 
-            controlnet = controlnet,
-            torch_dtype=torch.bfloat16, 
-            **kwargs
-    )
+    if args.pipeline_type == 'controlnet':
+        control_image = load_image(args.pose_path)
+        pipe = GaudiStableDiffusionZImageControlNetPipeline.from_pretrained(
+            model_name_path,
+            controlnet=controlnet,
+            torch_dtype=torch.bfloat16,
+            **kwargs,
+        )
+    elif args.pipeline_type == 'controlnet_inpaint':
+        image = load_image(args.image_path)
+        mask_image = load_image(args.mask_path)
+        control_image = load_image(args.pose_path)
+        pipe = GaudiStableDiffusionZImageControlNetInpaintPipeline.from_pretrained(
+                model_name_path, 
+                controlnet = controlnet,
+                torch_dtype=torch.bfloat16, 
+                **kwargs
+        )
+    else:
+        print(f'ERROR unsuppported pipeline type:{args.pipeline_type}')
+        exit()
     pipe.to("hpu")
-    image = load_image(args.image_path)
-    mask_image = load_image(args.mask_path)
-    control_image = load_image(args.pose_path)
-    print('load image done, inference...')
+
+    print('load image and pipeline done, inference...')
     
     for i in range(args.loop):
         t0 = time.time()
         # 2. Generate Image
-        image = pipe(
-            prompt=args.prompts,
-            image=image,
-            mask_image=mask_image,
-            control_image=control_image,
-            controlnet_conditioning_scale=args.controlnet_conditioning_scale,
-            height=args.height,
-            width=args.width,
-            num_inference_steps=args.num_inference_steps,
-            guidance_scale=args.guidance_scale,
-            generator=torch.Generator("cpu").manual_seed(args.seed),
-        ).images[0]
+        if args.pipeline_type == 'controlnet':
+            image_out = pipe(
+                prompt=args.prompts,
+                control_image=control_image,
+                controlnet_conditioning_scale=args.controlnet_conditioning_scale,
+                height=args.height,
+                width=args.width,
+                num_inference_steps=args.num_inference_steps,
+                guidance_scale=args.guidance_scale,
+                generator=torch.Generator("cpu").manual_seed(args.seed),
+            ).images[0]
+        elif args.pipeline_type == 'controlnet_inpaint':
+            image_out = pipe(
+                prompt=args.prompts,
+                image=image,
+                mask_image=mask_image,
+                control_image=control_image,
+                controlnet_conditioning_scale=args.controlnet_conditioning_scale,
+                height=args.height,
+                width=args.width,
+                num_inference_steps=args.num_inference_steps,
+                guidance_scale=args.guidance_scale,
+                generator=torch.Generator("cpu").manual_seed(args.seed),
+            ).images[0]
         torch.hpu.synchronize()
         t1 = time.time()
         duration = t1 - t0
-        print("Z-Image Controlnet inpaint Pipeline Latency in Loop #{:d}: {:.1f} sec".format(i, duration))
-    file_name = f"z_image_controlnet_inpaint_output_{args.width}x{args.height}.png"
-    image.save(file_name)
+        print(f"Z-Image {args.pipeline_type} Pipeline Latency in Loop #{i:d}: {duration:.1f} sec")
+    file_name = f"z_image_{args.pipeline_type}_{args.width}x{args.height}.png"
+    image_out.save(file_name)
     print(f'Completed saving {file_name}!')
 
 if "__main__" == __name__:
